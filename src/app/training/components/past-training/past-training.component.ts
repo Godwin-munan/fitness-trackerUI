afcb597712f784fe0a9bf1f2c23e8271b1dbd1cc
Godwin Munan <godwinjethro@gmail.com>
2023-06-16 15:50:55 +0100

    datatable impl v1

--- src/app/training/components/past-training/past-training.component.ts
+++ src/app/training/components/past-training/past-training.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exercise } from 'app/training/model/exercise.model';
@@ -12,9 +13,12 @@ import { TrainingService } from 'app/training/service/training.service';
 export class PastTrainingComponent implements OnInit, OnDestroy, AfterViewInit {
 
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
+  pageSizes = [5, 10, 25];
+  totalData: number = 0;
   dataSource = new MatTableDataSource<Exercise >()
 
   @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(
     private _trainingService: TrainingService,
@@ -22,14 +26,21 @@ export class PastTrainingComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngOnInit() {
    this.dataSource.data = this._trainingService.getCompletedOrCancelledExercises();
+   this.totalData = this.dataSource.data.length
   }
 
   //this method execute after the view finish rendering
   ngAfterViewInit() {
    this.dataSource.sort = this.sort;
+   this.dataSource.paginator = this.paginator;
+  }
+
+  doFilter(filterValue: string){
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   ngOnDestroy() {
    
   }
+
 }
